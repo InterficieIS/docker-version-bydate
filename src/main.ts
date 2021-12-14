968d9cb2eb391eb8e67546deef269af9f9fb6ddc
Marc Pujol <marc.pujol@interficie.com>
2021-12-14 06:33:48 +0100

    chore: update eslint rules

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import * as core from '@actions/core'
+import {getInput, setFailed, setOutput} from '@actions/core'
 import fetchCounter from './fetchCounter'
 
 async function run(): Promise<void> {
   try {
-    const image: string = core.getInput('image')
+    const image: string = getInput('image')
     const date = new Date().toISOString().substr(0, 10).split('-')
     const prefix = `${date[0]}.${date[1]}${date[2]}`
     const counter = await fetchCounter(image, prefix)
 
-    core.setOutput('version', `${prefix}.${counter}`)
+    setOutput('version', `${prefix}.${counter}`)
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed((error as Error).message)
   }
 }
 
@@ -19,5 +19,5 @@ run()
   .then(() => {})
   // eslint-disable-next-line github/no-then
   .catch(error => {
-    core.setFailed(error.message)
+    setFailed(error.message)
   })
