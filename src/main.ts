4f0b26d97353c05d47e3426bdeb3fcb6d38d3e3b
Marc Pujol <marc.pujol@interficie.com>
2022-12-07 08:11:58 +0100

    chore: update packages to latest versions

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import fetchCounter from './fetchCounter'
 async function run(): Promise<void> {
   try {
     const image: string = getInput('image')
-    const date = new Date().toISOString().substr(0, 10).split('-')
+    const date = new Date().toISOString().slice(0, 10).split('-')
     const prefix = `${date[0]}.${date[1]}${date[2]}`
     const counter = await fetchCounter(image, prefix)
 
