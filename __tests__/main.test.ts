70e310290a54b898739e65feadc62d5837e99359
Marc Pujol <marc.pujol@interficie.com>
2024-07-24 08:59:22 +0200

    chore: switch ci from npm to yarn

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,14 +3,16 @@ import * as cp from 'child_process'
 import * as path from 'path'
 
 it('runs an action with the env/stdout protocol', () => {
-  process.env['INPUT_IMAGE'] = 'interficie/helvetia-php'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptionsWithStringEncoding = {
-    env: process.env,
+    env: {
+      INPUT_IMAGE: 'interficie/helvetia-php'
+    },
     encoding: 'utf8'
   }
 
   const result = cp.execFileSync(np, [ip], options).trim()
-  expect(result).toMatch(/::set-output name=version::[0-9]{4}\.[0-9]{4}\.0/)
+  console.log(result)
+  expect(result).toMatch(/::set-output name=version::[0-9]{4}\.[0-9]{4}\.[0-9]/)
 })
